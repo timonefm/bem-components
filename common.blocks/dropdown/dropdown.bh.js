0e943704e5630fe0f6bebc6da2151a23e8fac88d
aristov <vv.aristov@gmail.com>
2015-03-06 15:47:09 +0300

    remove elems

--- common.blocks/dropdown/dropdown.bh.js
+++ common.blocks/dropdown/dropdown.bh.js
@@ -11,4 +11,34 @@ module.exports = function(bh) {
         ];
     });
 
+    bh.match('dropdown__popup', function(ctx, json) {
+        var popup = json.popup;
+
+        if(ctx.isSimple(popup) || popup.block !== 'popup') {
+            popup = { block : 'popup', content : popup };
+        }
+
+        var popupMods = popup.mods || (popup.mods = {});
+        popupMods.theme || (popupMods.theme = ctx.mod('theme'));
+        popupMods.hasOwnProperty('autoclosable') || (popupMods.autoclosable = true);
+
+        popupMods.target = 'anchor';
+
+        popup.mix = { block : 'dropdown', mods : ctx.tParam('mods'), js : { id : json.id } };
+
+        return popup;
+    });
+
+    bh.match('dropdown__switcher', function(ctx, json) {
+        var content = ctx.content();
+
+        content.block && (content.mix = {
+            block : 'dropdown',
+            mods : ctx.tParam('mods'),
+            js : { id : json.id }
+        });
+
+        return content;
+    });
+
 };
