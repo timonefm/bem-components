588abe05e8c92e5075f792c43f3f10dccffb9352
aristov <vv.aristov@gmail.com>
2015-03-06 20:13:17 +0300

    proper deps

--- common.blocks/dropdown/dropdown.deps.js
+++ common.blocks/dropdown/dropdown.deps.js
@@ -1,8 +1,6 @@
 [{
     mustDeps : { block : 'i-bem', elem : 'dom' },
     shouldDeps : [
-        { elem : 'switcher' },
-        { elem : 'popup' },
         {
             block : 'popup',
             mods : {
