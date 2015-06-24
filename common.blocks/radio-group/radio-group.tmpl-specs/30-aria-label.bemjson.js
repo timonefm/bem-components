6e663f7044ac6b5b934af00f97ba6ca5cccc6c1e
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2015-06-24 16:18:58 +0300

    radio-group: add tmpl-specs for block

---
+++ common.blocks/radio-group/radio-group.tmpl-specs/30-aria-label.bemjson.js
@@ -0,0 +1,9 @@
+({
+    block : 'radio-group',
+    name : 'radio-group_type_button',
+    ariaLabel : 'Group label',
+    options : [
+        { text : 'first', val : 1 },
+        { text : 'VK', val : 2 }
+    ]
+});
