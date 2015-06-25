b7ec26529a6e89f3ad4477053999ceb640c979ad
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2015-06-25 16:14:02 +0300

    checkbox: add specs

--- common.blocks/checkbox/checkbox.spec.js
+++ common.blocks/checkbox/checkbox.spec.js
@@ -1,7 +1,9 @@
 modules.define(
     'spec',
-    ['checkbox', 'i-bem__dom', 'jquery', 'BEMHTML'],
-    function(provide, Checkbox, BEMDOM, $, BEMHTML) {
+    ['checkbox', 'i-bem__dom', 'jquery', 'BEMHTML', 'chai'],
+    function(provide, Checkbox, BEMDOM, $, BEMHTML, chai) {
+
+var expect = chai.expect;
 
 describe('checkbox', function() {
     var checkbox;
@@ -46,6 +48,27 @@ describe('checkbox', function() {
                 .prop('checked', false)
                 .trigger('change');
             checkbox.hasMod('checked').should.be.false;
+        });
+
+        it('should properly update "aria-checked" attribute', function() {
+            checkbox
+                .setMod('checked')
+                .domElem.attr('aria-checked').should.be.equal('true');
+
+            checkbox
+                .delMod('checked')
+                .domElem.attr('aria-checked').should.be.equal('false');
+        });
+    });
+
+    describe('disabled', function () {
+        it('should properly update "aria-disabled" attribute', function () {
+            checkbox
+                .setMod('disabled')
+                .domElem.attr('aria-disabled').should.be.equal('true');
+
+            checkbox.delMod('disabled');
+            expect(checkbox.domElem.attr('aria-disabled')).to.be.undefined;
         });
     });
 });
